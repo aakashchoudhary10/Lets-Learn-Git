ab4ac18d6a5dd6d7e99ad3edf1a870f6ab47cfdd
Aakash Choudhary <aakash.choudhary45@gmail.com>
2022-12-31 22:49:22 +0530

    updated Diff.java message

--- Diff.java
+++ Diff.java
@@ -3,7 +3,7 @@ public class Sum {
 public static void main (String args[]){
     int  a = 5;
     int b = 6;
-    System.out.println("This is diff ": a-b); 
+    System.out.println("This is updated diff ": a-b); 
 
 }
 
